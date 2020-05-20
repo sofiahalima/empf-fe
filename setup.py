fd8171674e83326cf15ae0e33305fb4b68a9d9cc
Sofia Halima <sofiahalima@Sofias-MacBook-Pro.local>
2020-05-21 01:45:07 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='empf-fe',
+    version='1.0',
+    packages=['home_page', 'activities', 'activities.migrations'],
+    url='',
+    license='',
+    author='sofiahalima',
+    author_email='halima.sofia@gmail.com',
+    description=''
+)
